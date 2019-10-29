ad130288a58106de7608eb33dceb4c845fc59a3e
aslam <aslamcsebd@gmail.com>
2019-10-29 19:35:41 +0600

    Data binding...

---
+++ dataBinding.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Vue js tutorial</title>
+</head>
+<body>
+   <div id="abc">
+      <p>Enter your name...</p>
+      <input type="text" v-model="name">
+      <h2>My name is {{ name }}</h2>
+   </div>
+
+<script type="text/javascript" src="vue.js"></script>
+<script type="text/javascript">
+   var app=new Vue({
+      el:"#abc",
+      data:{
+         name: "Md Aslam" 
+      }
+   });
+</script>
+</body>
+</html>
