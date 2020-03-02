c61a57e665e335495070640517ecc5b7db13cd36
Md Aslam Hossain <aslamcsebd@gmail.com>
2020-03-03 00:45:23 +0600

    Index add

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Favorite Color</title>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- <meta http-equiv="refresh" content="5" /> -->
+      <!-- <link rel="stylesheet" href="assets/css/dataTables.min.css"> -->
+      <link rel="stylesheet" href="bootstrap.min.css">
+      <link rel="stylesheet" href="style.css">
+   </head>
+
+   <body>
+
+      <div class="container">
+         <h3>Vue-js</h3>
+
+         <ul>
+            <li><a class="btn btn-success" href="attributeBinding.php" target="_blink">attributeBinding</a></li>
+            <li><a class="btn btn-success" href="componentCommunications&CustomEvents.php" target="_blink">componentCommunications&CustomEvents</a></li>
+            <li><a class="btn btn-success" href="componentExample.php" target="_blink">componentExample</a></li>
+            <li><a class="btn btn-success" href="components.php" target="_blink">components</a></li>
+            <li><a class="btn btn-success" href="computedProperties.php" target="_blink">computedProperties</a></li>
+            <li><a class="btn btn-success" href="conditionalRendering.php" target="_blink">conditionalRendering</a></li>
+            <li><a class="btn btn-success" href="dataBinding.php" target="_blink">dataBinding</a></li>
+            <li><a class="btn btn-success" href="event_methods.php" target="_blink">event_methods</a></li>
+            <li><a class="btn btn-success" href="filter.php" target="_blink">filter</a></li>
+            <li><a class="btn btn-success" href="listRendering.php" target="_blink">listRendering</a></li>
+            <li><a class="btn btn-success" href="transitionAnimation.php" target="_blink">transitionAnimation</a></li>
+         </ul>
+
+         
+      </div>
+
+
+      <!-- All js -->
+
+      <script type="text/javascript" src="assets/js/jquery-3.4.1.min.js"></script>
+      <script type="text/javascript" src="assets/js/dataTables.min.js"></script>
+      <script type="text/javascript" src="assets/js/clipboard.min.js"></script> 
+      <script type="text/javascript" src="assets/js/bootstrap.js"></script>    
+      
+   </body>
+</html>
